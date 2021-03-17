8a4d3a436c01dfe8a51f973adf4583f8cae9e2ab
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-03-17 10:09:34 +0000

    Initial Version

---
+++ src/main/java/Student.java
@@ -0,0 +1,66 @@
+public class Student {
+    private int number;
+    private String fullName;
+    private int[] assignmentGrade;
+    private int[] assignmentPercentageAllocation;
+
+    Student(int number, String fullName, int[] assignmentGrade, int[] assignmentPercentageAllocation ){
+        this.number = number;
+        this.fullName = fullName;
+        this.assignmentGrade = assignmentGrade;
+        this.assignmentPercentageAllocation = assignmentPercentageAllocation;
+    }
+
+    private int calculateTotalGrade(int[] assignmentGrade, int[] percentageAllocation){
+        double totalGrade = 1.0;
+        int counter = 0;
+        for (int individualGrade:this.assignmentGrade){
+            totalGrade +=  (individualGrade * percentageAllocation[counter]/100);
+            counter +=  1;
+        }
+        return (int)totalGrade;
+    }
+
+    private String determineLetterGrade(int gradeMark){
+        String letterGrade;
+        final int maxMark = 22;
+        if ((maxMark - gradeMark) < 5) {
+            letterGrade = "A" + (int)((maxMark - gradeMark) + 1);
+        } else if ((maxMark - gradeMark) < 7) {
+            letterGrade = "B" + (int)((maxMark - gradeMark) -4);
+        } else if ((maxMark - gradeMark) < 10) {
+            letterGrade = "C" + (int)((maxMark - gradeMark) -7);
+        } else {
+            letterGrade = "H";
+        }
+        return letterGrade;
+    }
+
+    private String getFirstInitial(){
+        return fullName.substring(0,1);
+    }
+
+    private String getLastName(){
+        return fullName.split(" ")[1];
+    }
+
+    private void displayGrade(int totalGrade, String letterGrade){
+        System.out.println(totalGrade);
+        System.out.println( getFirstInitial()+ " " + getLastName() + " got " + letterGrade);
+    }
+    public void cg(){
+
+
+        int gradeTotal = calculateTotalGrade(assignmentGrade,assignmentPercentageAllocation);
+        String letterGrade = determineLetterGrade(gradeTotal);
+        displayGrade(gradeTotal, letterGrade);
+
+    }
+
+    public static void main(String[] arg){
+        int[] a = {15,19,18};
+        int[] b = {25,25,50};
+        St st = new St(20012, "Derek Somerville",a,b );
+        st.cg();
+    }
+}
---
+++ src/main/java/ToStart.java
@@ -0,0 +1,40 @@
+class St {
+    private int n;
+    private String fn;
+    private int[] g;
+    private int[] p;
+
+    St(int d, String n, int[] g, int[] p ){
+        this.n = d;
+        this.fn = n;
+        this.g = g;
+        this.p = p;
+    }
+    public void cg(){
+        double g = 1.0;
+        String sg;
+        int c = 0;
+        for (int m:this.g){
+            g = g + (m * p[c]/100);
+            c = c + 1;
+        }
+        if ((22 - g) < 5) {
+            sg = "A" + (int)((22 - g) + 1);
+        } else if ((22 - g) < 7) {
+            sg = "B" + (int)((22 - g) -4);
+        } else if ((22 - g) < 10) {
+            sg = "C" + (int)((22 - g) -7);
+        } else {
+            sg = "H";
+        }
+        System.out.println(g);
+        System.out.println(fn.substring(0,1) + " " + fn.split(" ")[1] + " got " + sg);
+    }
+
+    public static void main(String[] arg){
+        int[] a = {15,19,18};
+        int[] b = {25,25,50};
+        St st = new St(20012, "Derek Somerville",a,b );
+        st.cg();
+    }
+}
